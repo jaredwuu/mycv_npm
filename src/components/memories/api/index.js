ec2df263a0812d5a2927d0836b5568f5082ceca5
jaredwuu <jiachaowu@hotmail.com>
2021-07-11 15:51:43 -0400

    up

--- src/components/memories/api/index.js
+++ src/components/memories/api/index.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 
 // const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-const API = axios.create({ baseURL: 'https://jiachaowu.site/' })
+const API = axios.create({ baseURL: 'https://jiachaowu.com/' })
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
         req.headers.Authorization= `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
